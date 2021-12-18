8199be91a70026d6fc8666350273f2a4af847397
Phelps <phelpsmemo@gmail.com>
2021-12-18 07:48:25 -0300

    add proxy cache (Closes #28)

--- storm_project/proxies.py
+++ storm_project/proxies.py
@@ -5,9 +5,7 @@
 # storm-project is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-from flask import request
-
-from flask import current_app
+from flask import request, current_app
 from werkzeug.local import LocalProxy
 
 
@@ -24,11 +22,18 @@ def _current_project():
     project_obj = None
     project_id = request.view_args.get("project_id", None)
 
+    # caching to save the database.
+    # fixme: improve the relation between
+    # project and other entities.
+    if hasattr(g, "project"):
+        return g.project
+
     if project_id:
         project_obj = current_research_project_extension.research_project_service.read(
             project_id, g.identity
         )
 
+    g.project = project_obj
     return project_obj
 
 
